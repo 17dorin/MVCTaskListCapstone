58ec7eb77cc351652e9e2287d9ae49f2743e3a67
Nick D'Oria <17dorin@gmail.com>
2021-02-27 19:28:10 -0500

    CRUD db actions added, need to add search/filter functionality

---
+++ Controllers/ToDoCapstoneController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoMVC.Models;
+
+namespace ToDoMVC.Controllers
+{
+    public class ToDoCapstoneController : Controller
+    {
+        private readonly ToDoCapstoneContext _context;
+
+        public ToDoCapstoneController(ToDoCapstoneContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        public IActionResult AddToDo()
+        {
+            //PS, I would love some feedback on how to improve this method of getting the current logged-in user.
+            //I'm aware this this is a pretty hacky and ugly solution. Unfortunately, this is the only way
+            //I could get it to work properly
+            string id = User.Identity.Name;
+            List<AspNetUsers> loggedIn = _context.AspNetUsers.Include(x => x.ToDos).Where(x => x.UserName == id).ToList();
+            return View(loggedIn[0]);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddToDo(ToDos t)
+        {
+            if (t.DueDate == null)
+            {
+                t.DueDate = DateTime.MaxValue;
+            }
+            t.Completed = false;
+            List<AspNetUsers> loggedIn = _context.AspNetUsers.Where(x => x.UserName == User.Identity.Name).ToList();
+            t.UserId = loggedIn[0].Id;
+            _context.ToDos.Add(t);
+            _context.SaveChanges();
+
+            return RedirectToAction("AddToDo");
+        }
+
+        [Authorize]
+        public IActionResult UpdateCompletion(int Id)
+        {
+            ToDos toUpdate = _context.ToDos.Where(x => x.Id == Id).ToList()[0];
+            toUpdate.Completed = !toUpdate.Completed;
+            _context.SaveChanges();
+
+            return RedirectToAction("AddToDo");
+        }
+
+        [Authorize]
+        public IActionResult DeleteItem(int Id)
+        {
+            ToDos toUpdate = _context.ToDos.Where(x => x.Id == Id).ToList()[0];
+            TempData["Id"] = toUpdate.Id;
+
+            return View(toUpdate);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult DeleteItem(string option)
+        {
+            if (option.Equals("yes"))
+            {
+                ToDos toDelete = _context.ToDos.Where(x => x.Id == int.Parse(TempData["Id"].ToString())).ToList()[0];
+                _context.ToDos.Remove(toDelete);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("AddToDo");
+        }
+    }
+}
---
+++ Models/ToDos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ToDoMVC.Models
+{
+    public partial class ToDos
+    {
+        public int Id { get; set; }
+        public string Details { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool Completed { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
